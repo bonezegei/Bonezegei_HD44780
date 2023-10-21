a93e97289b340ed5b650ff0456b5f87b918a3c1f
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-22 06:23:45 +0800

    Update Bonezegei_HD44780.h

--- src/Bonezegei_HD44780.h
+++ src/Bonezegei_HD44780.h
@@ -1,3 +1,4 @@
+#include <stdint.h>
 /*
   This Library is written for HD44780 LCD
   Author: Bonezegei (Jofel Batutay)
@@ -9,12 +10,29 @@
 
 class Bonezegei_HD44780 {
 public:
-  //parameters that corresponds to the LCD 
+  //parameters that corresponds to the LCD
   //R/W is set to ground since this library does not read any register to the LCD
   Bonezegei_HD44780(int8_t rs, int8_t en, int8_t d4, int8_t d5, int8_t d6, int8_t d7);
-  
+
   // initialize the Display
   void begin();
+
+  //clock effect
+  void latch();
+
+  // set the last nibble to the Data pins
+  void write4Bit(uint8_t data);
+
+  // set 8 bit
+  void writeByte(uint8_t data);
+
+  void writeCommand4(uint8_t data);
+  void writeCommand(uint8_t data);
+  void writeData(uint8_t data);
+
+
+  void setCursor(uint8_t x, uint8_t y);
+
 
   //pin Values in private
 private:
