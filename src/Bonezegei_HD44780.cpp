190e6902547a02cd750732b886383eeb709ff4b8
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-22 07:09:01 +0800

    Update Bonezegei_HD44780.cpp

--- src/Bonezegei_HD44780.cpp
+++ src/Bonezegei_HD44780.cpp
@@ -12,6 +12,21 @@ Bonezegei_HD44780::Bonezegei_HD44780(int8_t rs, int8_t en, int8_t d4, int8_t d5,
   _d5 = d5;
   _d6 = d6;
   _d7 = d7;
+  mode = HD44780_4BIT;
+}
+
+Bonezegei_HD44780::Bonezegei_HD44780(int8_t rs, int8_t en, int8_t d0, int8_t d1, int8_t d2, int8_t d3, int8_t d4, int8_t d5, int8_t d6, int8_t d7) {
+  _rs = rs;
+  _en = en;
+  _d0 = d0;
+  _d1 = d1;
+  _d2 = d2;
+  _d3 = d3;
+  _d4 = d4;
+  _d5 = d5;
+  _d6 = d6;
+  _d7 = d7;
+  mode = HD44780_8BIT;
 }
 
 void Bonezegei_HD44780::begin() {
@@ -21,37 +36,48 @@ void Bonezegei_HD44780::begin() {
   pinMode(_d5, OUTPUT);
   pinMode(_d6, OUTPUT);
   pinMode(_d7, OUTPUT);
-  delayMicroseconds(50000);
-
+  delay(50);
   digitalWrite(_rs, LOW);
   digitalWrite(_en, LOW);
 
-  write4Bit(0x03);
-  delayMicroseconds(4500);  // wait min 4.1ms
+  if (mode == HD44780_8BIT) {
+    pinMode(_d0, OUTPUT);
+    pinMode(_d1, OUTPUT);
+    pinMode(_d2, OUTPUT);
+    pinMode(_d3, OUTPUT);
+    write8Bit(0x30);
+    delay(5);
+    write8Bit(0x30);
+    delayMicroseconds(150);
+    write8Bit(0x30);
+    write8Bit(0x3C);
+    write8Bit(0x08);
+    write8Bit(0x01);
+    write8Bit(0x07);
+    write8Bit(0x0F);
+    writeData('A');
+  } else {
 
-  // second try
-  write4Bit(0x03);
-  delayMicroseconds(4500);  // wait min 4.1ms
+    write4Bit(0x03);
+    delayMicroseconds(4500);
+    write4Bit(0x03);
+    delayMicroseconds(4500);
+    write4Bit(0x03);
+    delayMicroseconds(150);
+    write4Bit(0x02);
 
-  // third go!
-  write4Bit(0x03);
-  delayMicroseconds(150);
+    write4Bit(0x02);
+    write4Bit(0x0C);
+    write4Bit(0x00);
+    write4Bit(0x08);
+    write4Bit(0x00);
+    write4Bit(0x01);
+    write4Bit(0x00);
+    write4Bit(0x06);
 
-  // finally, set to 4-bit interface
-  write4Bit(0x02);
-
-  writeByte(0x2C);  //set Display 2 line 5x20 dots
-  writeByte(0x08);
-  writeByte(0x01);
-  writeByte(0x07);
-
-  writeByte(0x0F);
-
-  writeData('A');
-  writeData('A');
-  writeData('A');
-
-
+    write4Bit(0x00);
+    write4Bit(0x0F);
+  }
 }
 
 void Bonezegei_HD44780::latch() {
@@ -91,6 +117,56 @@ void Bonezegei_HD44780::write4Bit(uint8_t data) {
   latch();
 }
 
+void Bonezegei_HD44780::write8Bit(uint8_t data) {
+  if (data & 0x1) {
+    digitalWrite(_d0, HIGH);
+  } else {
+    digitalWrite(_d0, LOW);
+  }
+
+  if (data & 0x2) {
+    digitalWrite(_d1, HIGH);
+  } else {
+    digitalWrite(_d1, LOW);
+  }
+
+  if (data & 0x4) {
+    digitalWrite(_d2, HIGH);
+  } else {
+    digitalWrite(_d2, LOW);
+  }
+
+  if (data & 0x8) {
+    digitalWrite(_d3, HIGH);
+  } else {
+    digitalWrite(_d3, LOW);
+  }
+  if (data & 0x10) {
+    digitalWrite(_d4, HIGH);
+  } else {
+    digitalWrite(_d4, LOW);
+  }
+
+  if (data & 0x20) {
+    digitalWrite(_d5, HIGH);
+  } else {
+    digitalWrite(_d5, LOW);
+  }
+
+  if (data & 0x40) {
+    digitalWrite(_d6, HIGH);
+  } else {
+    digitalWrite(_d6, LOW);
+  }
+
+  if (data & 0x80) {
+    digitalWrite(_d7, HIGH);
+  } else {
+    digitalWrite(_d7, LOW);
+  }
+  latch();
+}
+
 
 
 void Bonezegei_HD44780::writeByte(uint8_t data) {
@@ -105,9 +181,17 @@ void Bonezegei_HD44780::writeCommand4(uint8_t data) {
 
 void Bonezegei_HD44780::writeCommand(uint8_t data) {
   digitalWrite(_rs, LOW);
-  writeByte(data);
+  if (mode == HD44780_4BIT) {
+    writeByte(data);
+  } else {
+    write8Bit(data);
+  }
 }
 void Bonezegei_HD44780::writeData(uint8_t data) {
   digitalWrite(_rs, HIGH);
-  writeByte(data);
+  if (mode == HD44780_4BIT) {
+    writeByte(data);
+  } else {
+    write8Bit(data);
+  }
 }
