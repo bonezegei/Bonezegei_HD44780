cb9c848b5aeb872664f2b9ae22ddbefdb3189ab1
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-10-21 19:21:46 +0800

    Update Bonezegei_HD44780.h

--- src/Bonezegei_HD44780.h
+++ src/Bonezegei_HD44780.h
@@ -9,12 +9,14 @@
 
 class Bonezegei_HD44780 {
 public:
-
+  //parameters that corresponds to the LCD 
+  //R/W is set to ground since this library does not read any register to the LCD
   Bonezegei_HD44780(int8_t rs, int8_t en, int8_t d4, int8_t d5, int8_t d6, int8_t d7);
-
+  
+  // initialize the Display
   void begin();
 
-
+  //pin Values in private
 private:
   int8_t _rs;
   int8_t _en;
@@ -23,6 +25,5 @@ private:
   int8_t _d6;
   int8_t _d7;
 };
-
 
 #endif
