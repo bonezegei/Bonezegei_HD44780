c508d2f8f41f412e3f4db1b799ad68087755c6d4
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-19 20:28:27 +0800

    Update Clear.ino
    Clear example added

--- examples/Clear/Clear.ino
+++ examples/Clear/Clear.ino
@@ -1,1 +1,36 @@
+/*
+  Clear Display
+  Author: Bonezegei (Jofel Batutay)
+  Date: November 2024
+*/
 
+#include <Bonezegei_HD44780.h>
+
+/*
+  param1 = RS
+  param2 = EN
+  param3 = D4
+  param3 = D5
+  param3 = D6
+  param3 = D7
+*/
+Bonezegei_HD44780 lcd(9, 8, 4, 5, 6, 7);
+
+void setup() {
+  lcd.begin();
+  lcd.print("Bonezegei");
+  lcd.setPosition(0, 1);      //param1 = X   param2 = Y
+  lcd.print("HD44780");
+  delay(2000);
+  lcd.clear(); 
+  lcd.setPosition(0, 0);
+  lcd.print("Clear");
+}
+
+void loop() {
+  delay(1000);
+  lcd.setPosition(0, 0);
+  lcd.print("Clear");
+  delay(1000);
+  lcd.clear();            
+}
